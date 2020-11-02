aae103a161cc066bc0c63b83f4a15d560ab0dc83
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-11-02 11:30:46 -0800

    Added config file

--- bloglist/app.js
+++ bloglist/app.js
@@ -4,8 +4,9 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
 const blogsRouter = require('./controllers/blog')
+const config = require('dotenv').config()
 
-const mongoUrl = 'mongodb+srv://oftUser:efg293@cluster0.0k8sa.mongodb.net/blogs?retryWrites=true&w=majority'
+const mongoUrl = config.MONGODB_URI
 
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
--- bloglist/index.js
+++ bloglist/index.js
@@ -2,8 +2,9 @@ const app = require('./app')
 const http = require('http')
 const server = http.createServer(app) 
 const logger = require('./utils/logger')
+const config = require('dotenv').config()
 
 const PORT = 3003
 server.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
