f98ca1a5faf615573c35b515c9b427ca657cbaac
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-11-04 17:43:04 -0800

    Added testing for api POST

--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -16,6 +16,25 @@ test('there are three posts', async () => {
     expect(response.body).toHaveLength(3)
 })
 
+test('api posts new data', async () => {
+    const blog = {
+        "title": "api", 
+        "author": "post",
+        "url": "test",
+        "likes": 123
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(blog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    // const response = await api.get('/api/blogs')
+    // const contents = response.body.map(r => r.content)
+
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
