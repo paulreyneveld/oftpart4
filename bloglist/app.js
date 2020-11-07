1289cfb9752596c9ba93e3a2476d62c3fa350cc5
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-11-07 13:01:33 -0800

    Added support for JWT authentication

--- bloglist/app.js
+++ bloglist/app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const Blog = require('./models/blog')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoUrl = config.MONGODB_URI
 
@@ -16,6 +17,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 
 module.exports = app
